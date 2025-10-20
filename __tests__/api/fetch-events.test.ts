cad03303eb065d96cc9df10df65ccfd342e91c3f
craig-sinclair <craig.a.sinclair04@gmail.com>
2025-10-20 12:16:44 +0100

    Amend fetch-events to accept a timeline ID argument, and appropriate validation

--- __tests__/api/fetch-events.test.ts
+++ __tests__/api/fetch-events.test.ts
@@ -2,10 +2,18 @@ import { describe, it, expect } from "vitest";
 import { GET } from "@/app/api/fetch-events/route"
 
 describe("Fetch events test suite", () => {
-    it("Should initially give hello world message", async () => {
+    it("Should give appropriate error message when no timeline ID give", async () => {
         const response = await GET();
         const data = await response.json();
 
-        expect(data.message).toEqual("hello world")
+        expect(data.message).toEqual("No timeline ID provided.");
+    });
+
+    it("Should give hello message with appropriate timeline", async () => {
+        const exampleTimelineID: String = "test-id-123"
+        const response = await GET(exampleTimelineID);
+        const data = await response.json();
+
+        expect(data.message).toEqual(`Given timelineID: ${exampleTimelineID}`);
     })
 })
--- src/app/api/fetch-events/route.ts
+++ src/app/api/fetch-events/route.ts
@@ -1,6 +1,13 @@
 import { NextResponse } from "next/server"
 
-export async function GET() {
-    const response = {"message": "hello world"};
+export async function GET(timelineID?: String) {
+
+    if (!timelineID) {
+        const response = {
+            "message": "No timeline ID provided."
+        };
+        return NextResponse.json(response);
+    }
+    const response = {"message": `Given timelineID: ${timelineID}`};
     return NextResponse.json(response);
 }
