4a31868f9a6fe049f8528fd4fdb224dfcd7560a5
craig-sinclair <craig.a.sinclair04@gmail.com>
2025-10-21 12:35:41 +0100

    Update unit testing for fetch-events endpoint
    - Added cases for valid timeline but: empty event data, single event
      data response, and multiple events data response
    - For all, ensure valid response matching expected return value for
      events

--- __tests__/api/fetch-events.test.ts
+++ __tests__/api/fetch-events.test.ts
@@ -1,6 +1,7 @@
 import { describe, it, expect, vi, beforeEach } from "vitest";
 import { NextRequest } from "next/server";
-
+import { TimelineData } from "@/app/models/timeline";
+import { EventData } from "@/app/models/event"
 
 vi.mock("@/app/lib/mongoose", () => ({
     dbConnect: vi.fn(),
@@ -25,6 +26,14 @@ vi.mock("mongoose", async () => {
 
 import { GET } from "@/app/api/fetch-events/[timelineID]/route"
 
+const mockExpectedTimelineData: TimelineData = { _id: "1010", title: "test2", events: ["12345"], discussionID: 2020 };
+const mockSingleEventData: EventData[] = [{ _id: "12345", overview: "example", dateTime: "2025-01-01 10:00", relevance: 0.5, URLS: [], tags: [] }];
+
+const mockMultipleEventData: EventData[] = [
+    { _id: "12345", overview: "example", dateTime: "2025-01-01 10:00", relevance: 0.5, URLS: [], tags: [] },
+    { _id: "24680", overview: "example-2", dateTime: "2025-01-02 10:01", relevance: 0.9, URLS: ["example-123"], tags: ["example-tag"] },
+];
+
 describe("Fetch events test suite", () => {
     beforeEach(() => {
         vi.clearAllMocks();
@@ -43,8 +52,8 @@ describe("Fetch events test suite", () => {
     it("Should give appropriate error message for a timeline ID that does not exist", async () => {
         mockLean.mockResolvedValueOnce(null); // mock to not find matching timeline object
 
-        const mockRequest = {} as NextRequest
-        const response = await GET(mockRequest, ({ params: Promise.resolve({ timelineID: "example-id" }) }))
+        const mockRequest = {} as NextRequest;
+        const response = await GET(mockRequest, ({ params: Promise.resolve({ timelineID: "example-id" }) }));
         const data = await response.json();
 
         expect(data.success).toEqual(false);
@@ -55,12 +64,47 @@ describe("Fetch events test suite", () => {
     it("Should handle Mongo throwing database errors with appropriate return", async () => {
         mockLean.mockRejectedValue(new Error("DB Error"));
 
-        const mockRequest = {} as NextRequest
-        const response = await GET(mockRequest, ({ params: Promise.resolve({ timelineID: "example-id" }) }))
+        const mockRequest = {} as NextRequest;
+        const response = await GET(mockRequest, ({ params: Promise.resolve({ timelineID: "example-id" }) }));
         const data = await response.json();
 
         expect(data.success).toEqual(false);
         expect(data.error).toEqual("Failed to retrieve events from database");
         expect(data.details).toEqual("DB Error");   
+    });
+
+    it("Should correctly return empty event data for valid timeline", async () => {
+        mockLean.mockResolvedValue([]).mockResolvedValueOnce(mockExpectedTimelineData)
+        const mockRequest = {} as NextRequest;
+        const response = await GET(mockRequest, ({ params: Promise.resolve({timelineID: "example-id"})}));
+        const data = await response.json();
+
+        expect(mockLean).toHaveBeenCalled();
+        expect(data.success).toEqual(true);
+        expect(data.events).toEqual([]);
+    });
+
+    it("Should correctly return data for a single event for valid timeline", async () => {
+        mockLean.mockResolvedValue([]).mockResolvedValueOnce(mockExpectedTimelineData).mockResolvedValueOnce(mockSingleEventData);
+
+        const mockRequest = {} as NextRequest;
+        const response = await GET(mockRequest, ({params: Promise.resolve({timelineID: "exmaple-id"})}));
+        const data = await response.json();
+
+        expect(mockLean).toHaveBeenCalledTimes(2); // calls for timeline and event
+        expect(data.success).toEqual(true);
+        expect(data.events).toEqual(mockSingleEventData);
+    });
+
+    it("Should correctly return data for multiple events in valid timeline", async () => {
+        mockLean.mockResolvedValue([]).mockResolvedValueOnce(mockExpectedTimelineData).mockResolvedValueOnce(mockMultipleEventData);
+
+        const mockRequest = {} as NextRequest;
+        const response = await GET(mockRequest, ({params: Promise.resolve({timelineID: "exmaple-id"})}));
+        const data = await response.json();
+
+        expect(mockLean).toHaveBeenCalledTimes(2);
+        expect(data.success).toEqual(true);
+        expect(data.events).toEqual(mockMultipleEventData);
     });
 })
