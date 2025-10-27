5101b8e9776484503f5d3f4128265d80d5767ea0
Craig Sinclair <craig.a.sinclair04@gmail.com>
2025-10-27 19:04:41 +0000

    Added title and filtering options to event page

--- src/app/timeline/[timelineID]/page.tsx
+++ src/app/timeline/[timelineID]/page.tsx
@@ -39,17 +39,53 @@ export default function TimelinePage() {
     if (error) return <p>Error fetching events: {error}</p>;
 
     return (
-        <div className="p-6 space-y-4">
+        <div>
+            {/* Timeline title header */}
+            <div className="flex justify-center items-center w-full mb-5 md:mb-8">
+                <h1 className="text-3xl"> <span className="font-bold text-blue-500">Brexit Campaign</span> Event Timeline</h1>
+            </div>
+
+            {/* Event filter options */}
+            <div className="flex justify-between items-center mb-5 md:mb-10 max-w-full md:max-w-4/5 lg:max-w-3/4 ml-auto mr-auto">
+                <div>
+                    <label className="block mb-2 text-xs md:text-sm">Date Range</label>
+                    <input type="text"className="text-xs md:text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:focus:ring-blue-500 dark:focus:border-blue-500 border border-[var(--borderColour)]"
+                        placeholder="All Time" />
+                </div>
+
+                <div>
+                    <label className="block mb-2 text-xs md:text-sm">Tags</label>
+                    <input type="text" className="text-xs md:text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:focus:ring-blue-500 dark:focus:border-blue-500 border border-[var(--borderColour)]"
+                        placeholder="View All" />
+                </div>
+
+                <div>
+                    <label className="block mb-2 text-xs md:text-sm">Min. Relevance</label>
+                    <input type="text" className="text-xs md:text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:focus:ring-blue-500 dark:focus:border-blue-500 border border-[var(--borderColour)]"
+                        placeholder="High" />
+                </div>
+
+                <div>
+                    <label className="block mb-2 text-xs md:text-sm">Sort By</label>
+                    <input type="text" className="text-xs md:text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:focus:ring-blue-500 dark:focus:border-blue-500 border border-[var(--borderColour)]"
+                        placeholder="Chronological" />
+                </div>
+            </div>
+
+
             {events.length === 0 ? (
                 <p>No events found for this timeline.</p>
             ) : (
-                <ul className="space-y-2">
-                    {events.map((event) => (
-                        <li key={event._id} className="p-4 rounded-lg shadow bg-gray-50 dark:bg-gray-800">
-                            <h2 className="text-lg font-semibold">{event.overview}</h2>
-                        </li>
-                    ))}
-                </ul>
+                <>
+                    <ul className="space-y-2">
+                        {events.map((event) => (
+                            <li key={event._id} className="p-4 rounded-lg shadow bg-gray-50 dark:bg-gray-800">
+                                <h2 className="text-lg font-semibold">{event.overview}</h2>
+                            </li>
+                        ))}
+                    </ul>
+                </>
+
             )}
         </div>
     );
