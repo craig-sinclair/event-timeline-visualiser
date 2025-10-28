d5111a61705f1c076f0cee3fab5580f7e3796b81
craig-sinclair <craig.a.sinclair04@gmail.com>
2025-10-28 15:42:57 +0000

    Update event card styling
    - Remove full height CSS property for redundant vertical spacing created
      at bottom of the page
    - Update to include subtle hover effect (opacity decrease and pointer
      cursor) with transitions; for clicking to view event modal
      interactivity
    - Update corner radius of event cards to be smaller for large displays

--- src/app/components/HorizontalTimeline.tsx
+++ src/app/components/HorizontalTimeline.tsx
@@ -4,7 +4,7 @@ import { EventData } from "@/app/models/event";
 
 export default function HorizontalTimeline({ events }: { events: EventData[] }) {
   return (
-    <div className="mx-auto relative min-h-screen px-2 xs:px-4 sm:px-6 md:px-10 lg:px-16 xl:px-20 2xl:px-30 pt-12 xs:pt-16 sm:pt-24 md:pt-32 lg:pt-50 pb-12 xs:pb-16 sm:pb-24 md:pb-32 lg:pb-48">
+    <div className="mx-auto relative px-2 xs:px-4 sm:px-6 md:px-10 lg:px-16 xl:px-20 2xl:px-30 pt-12 xs:pt-16 sm:pt-24 md:pt-32 lg:pt-50 pb-12 xs:pb-16 sm:pb-24 md:pb-32 lg:pb-48">
       <div className="relative flex justify-between items-center gap-4 xs:gap-6 sm:gap-8 md:gap-12 lg:gap-16 xl:gap-20 2xl:gap-24">
         {/* Horizontal Base Line */}
         <div className="absolute left-0 right-0 h-[1px] bg-[var(--foreground)]" />
@@ -21,7 +21,7 @@ export default function HorizontalTimeline({ events }: { events: EventData[] }) 
 
             {/* Alternate between events above/below baseline */}
             <div
-              className={`absolute w-28 xs:w-32 sm:w-40 md:w-48 lg:w-56 xl:w-64 2xl:w-72 text-left border rounded-lg sm:rounded-xl p-2 sm:p-2.5 md:p-3 lg:p-4 shadow-sm backdrop-blur-sm ${
+              className={`absolute w-28 xs:w-32 sm:w-40 md:w-48 lg:w-56 xl:w-64 2xl:w-72 text-left border rounded-md sm:rounded-xs p-2 sm:p-2.5 md:p-3 lg:p-4 hover:opacity-75 transition-all duration-200 cursor-pointer ${
                 i % 2 === 0 
                   ? "bottom-full mb-6 xs:mb-8 sm:mb-10 md:mb-12 lg:mb-16 xl:mb-20" 
                   : "top-full mt-6 xs:mt-8 sm:mt-10 md:mt-12 lg:mt-16 xl:mt-20"
