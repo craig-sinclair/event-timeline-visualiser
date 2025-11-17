f4683441aa1401bee80933f9154d7ee25ea0ab8c
craig-sinclair <craig.a.sinclair04@gmail.com>
2025-11-17 12:01:19 +0000

    Add lib helper function to get timeline from ID
    - Using existing implemented API route, define helper function (for
      frontend robust use) to call endpoint and return timeline data
    - Add corresponding unit testing (logic copied from tests for helper
      function to get all timelines)

---
+++ src/app/lib/api/getTimelineFromId.ts
@@ -0,0 +1,11 @@
+import { TimelineResponse } from "@/app/models/timeline";
+
+export const getTimelineFromId = async ({ timelineID }: { timelineID: string }) => {
+	const response = await fetch(`/api/fetch-timeline/${timelineID}`);
+	const data: TimelineResponse = await response.json();
+
+	if (!data.success) {
+		throw new Error(data.error);
+	}
+	return data.timelines;
+};
