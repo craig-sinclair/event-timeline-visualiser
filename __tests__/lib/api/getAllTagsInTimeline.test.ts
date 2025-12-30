c8423221f113f273e6cb34cc46f13af6ab06d86c
craig-sinclair <craigsincy152@gmail.com>
2025-12-30 13:45:31 +0000

    add lint fixes for getAllTagsInTimeline function and tests

--- __tests__/lib/api/getAllTagsInTimeline.test.ts
+++ __tests__/lib/api/getAllTagsInTimeline.test.ts
@@ -1,5 +1,6 @@
-import { getAllTagsInTimeline } from "@/app/lib/getAllTagsInTimeline";
 import { describe, it, expect } from "vitest";
+
+import { getAllTagsInTimeline } from "@/app/lib/getAllTagsInTimeline";
 import { EventData } from "@/app/models/event";
 
 describe("Get all tags in timeline tests", () => {
@@ -8,42 +9,46 @@ describe("Get all tags in timeline tests", () => {
 		expect(result).toEqual([]);
 	});
 
-    it("Correctly return array of tags for single event with single tag", () => {
-        const singleEventData: EventData[] = [{
-            _id: "12345",
-            overview: "test",
-            dateTime: "2025-01-01",
-            furtherDescription: "test",
-            relevance: 0.5,
-            URLs: [],
-            tags: ["tag1"]
-        }];
+	it("Correctly return array of tags for single event with single tag", () => {
+		const singleEventData: EventData[] = [
+			{
+				_id: "12345",
+				overview: "test",
+				dateTime: "2025-01-01",
+				furtherDescription: "test",
+				relevance: 0.5,
+				URLs: [],
+				tags: ["tag1"],
+			},
+		];
 
-        const result = getAllTagsInTimeline({ eventsArray: singleEventData });
-        expect(result).toEqual(["tag1"]);
-    });
+		const result = getAllTagsInTimeline({ eventsArray: singleEventData });
+		expect(result).toEqual(["tag1"]);
+	});
 
-    it("Correctly returns array of tags for multiple events with multiple tags", () => {
-        const multipleEventsData: EventData[] = [{
-            _id: "12345",
-            overview: "test",
-            dateTime: "2025-01-01",
-            furtherDescription: "test",
-            relevance: 0.5,
-            URLs: [],
-            tags: ["tag1", "tag2", "tag3"]
-        },
-        {
-            _id: "123456",
-            overview: "test2",
-            dateTime: "2025-01-01",
-            furtherDescription: "test2",
-            relevance: 0.5,
-            URLs: [],
-            tags: ["tag4", "tag5"]          
-        }];
+	it("Correctly returns array of tags for multiple events with multiple tags", () => {
+		const multipleEventsData: EventData[] = [
+			{
+				_id: "12345",
+				overview: "test",
+				dateTime: "2025-01-01",
+				furtherDescription: "test",
+				relevance: 0.5,
+				URLs: [],
+				tags: ["tag1", "tag2", "tag3"],
+			},
+			{
+				_id: "123456",
+				overview: "test2",
+				dateTime: "2025-01-01",
+				furtherDescription: "test2",
+				relevance: 0.5,
+				URLs: [],
+				tags: ["tag4", "tag5"],
+			},
+		];
 
-        const result = getAllTagsInTimeline({ eventsArray: multipleEventsData });
-        expect(result).toEqual(["tag1", "tag2", "tag3", "tag4", "tag5"]);
-    })
+		const result = getAllTagsInTimeline({ eventsArray: multipleEventsData });
+		expect(result).toEqual(["tag1", "tag2", "tag3", "tag4", "tag5"]);
+	});
 });
--- src/app/lib/getAllTagsInTimeline.ts
+++ src/app/lib/getAllTagsInTimeline.ts
@@ -1,9 +1,9 @@
 import { EventData } from "@/app/models/event";
 
 export function getAllTagsInTimeline({ eventsArray }: { eventsArray: EventData[] }) {
-    const allTags: string[] = [];
-    for (let event of eventsArray) {
-        allTags.push(...event.tags);
-    }
-    return allTags;
+	const allTags: string[] = [];
+	for (const event of eventsArray) {
+		allTags.push(...event.tags);
+	}
+	return allTags;
 }
