9a0c274ccd406e573479afe7a1c52f5010fd96cc
craig-sinclair <craigsincy152@gmail.com>
2026-01-26 17:46:03 +0000

    Implement fetches of children topics using graphLookup
    Use Mongo graphLookup to recursively fetch complete (all
    child/grand-child, etc deep nested) children from a base topic.
    Utilises broader array as a connection field (if parent topic in
    another's broader field).
    Return unique (using set operator) array of qcodes with medtop prefix
    stripped.

--- __tests__/lib/getAllChildTopics.test.ts
+++ __tests__/lib/getAllChildTopics.test.ts
@@ -4,13 +4,14 @@ vi.mock("@/lib/mongoose", () => ({
 
 vi.mock("@/models/ontology", () => ({
 	OntologyTopic: {
-		findOne: vi.fn(),
+		aggregate: vi.fn(),
+		collection: {
+			name: "ontology",
+		},
 	},
 }));
 
 import { describe, it, expect, vi, type Mock, beforeEach } from "vitest";
-
-import { createMockChain } from "../setupTest";
 
 import { getAllChildTopics } from "@/lib/getAllChildTopics";
 import { OntologyTopic } from "@/models/ontology";
@@ -23,10 +24,31 @@ describe("Get all child topics test", () => {
 	it("Correctly handles empty topic ID parameter", async () => {
 		const result = await getAllChildTopics({ topicID: "" });
 		expect(result).toEqual([]);
+		expect(OntologyTopic.aggregate).not.toHaveBeenCalled();
 	});
 
 	it("Correctly handles case where base topic cannot be found", async () => {
-		(OntologyTopic.findOne as Mock).mockReturnValue(createMockChain(null));
+		(OntologyTopic.aggregate as Mock).mockResolvedValue([]);
 		await expect(getAllChildTopics({ topicID: "999" })).rejects.toThrowError();
+	});
+
+	it("Correctly handles topic with no child (narrower) topics", async () => {
+		(OntologyTopic.aggregate as Mock).mockResolvedValue([{ childQcodes: [] }]);
+		const result = await getAllChildTopics({ topicID: "test" });
+		expect(result).toEqual([]);
+	});
+
+	it("Correctly handles topic with single child (narrower) topic", async () => {
+		(OntologyTopic.aggregate as Mock).mockResolvedValue([{ childQcodes: ["medtop:12345"] }]);
+		const result = await getAllChildTopics({ topicID: "test" });
+		expect(result).toEqual(["12345"]);
+	});
+
+	it("Correctly handles multiple nested children results, removing duplicates", async () => {
+		(OntologyTopic.aggregate as Mock).mockResolvedValue([
+			{ childQcodes: ["medtop:12345", "medtop:12345", "medtop:2468", "medtop:999"] },
+		]);
+		const result = await getAllChildTopics({ topicID: "test" });
+		expect(result).toEqual(["12345", "2468", "999"]);
 	});
 });
--- src/lib/getAllChildTopics.ts
+++ src/lib/getAllChildTopics.ts
@@ -1,21 +1,44 @@
 import { dbConnect } from "@/lib/mongoose";
 import { OntologyTopic } from "@/models/ontology";
-import { TopicData } from "@/models/ontology.types";
 
-export async function getAllChildTopics({ topicID }: { topicID: string }) {
+export async function getAllChildTopics({ topicID }: { topicID: string }): Promise<string[]> {
 	if (topicID === "") {
 		return [];
 	}
 	await dbConnect();
 
 	const TOPIC_QCODE_PREFIX = "medtop:";
-	const baseTopic: TopicData | null = await OntologyTopic.findOne({
-		qcode: `${TOPIC_QCODE_PREFIX}${topicID}`,
-	}).lean<TopicData>();
+	const baseQcode = `${TOPIC_QCODE_PREFIX}${topicID}`;
 
-	if (!baseTopic) {
-		throw new Error(`Could not find topic with ID: ${topicID}.`);
+	/*
+	Recursively finds all children topics from base using MongoDB's graphLookup
+	Traverses, matching by broader field of other topics (including deeply nested children)
+	*/
+	const result = await OntologyTopic.aggregate([
+		{
+			$match: { qcode: baseQcode },
+		},
+		{
+			$graphLookup: {
+				from: "ontology",
+				startWith: "$qcode",
+				connectFromField: "qcode",
+				connectToField: "broader",
+				as: "descendants",
+			},
+		},
+		{
+			$project: {
+				childQcodes: `$descendants.qcode`,
+			},
+		},
+	]);
+
+	if (!result.length) {
+		throw new Error(`Could not find topic with ID: ${topicID}`);
 	}
+	const allChildTopics: string[] = result[0].childQcodes || [];
 
-	return [];
+	// Remove duplicates and medtop prefix
+	return [...new Set(allChildTopics)].map((childTopic) => childTopic.replace(/^medtop:/, ""));
 }
