6ef0c1bc42bf48a086793544549f6977357f1793
craig-sinclair <craigsincy152@gmail.com>
2026-01-28 20:34:52 +0000

    Add theme toggle component for dark/light mode toggle in navbar

--- src/components/ui/navbar.tsx
+++ src/components/ui/navbar.tsx
@@ -4,6 +4,8 @@ import Link from "next/link";
 import { useSession } from "next-auth/react";
 import { useState } from "react";
 
+import ThemeToggle from "@/components/ui/ThemeToggle";
+
 interface HeaderProps {
 	title: string;
 	isSignedIn: boolean;
@@ -32,39 +34,43 @@ export default function Navbar({ title, isSignedIn, onSignOut }: HeaderProps) {
 				</h1>
 			</Link>
 
-			{isSignedIn ? (
-				<div className="relative">
-					<button
-						onClick={toggleDropdown}
-						className="w-10 h-10 rounded-full bg-blue-600 text-white flex items-center justify-center font-bold"
-					>
-						{userInitials}
-					</button>
+			<div className="flex align-middle gap-2 items-center">
+				<ThemeToggle />
 
-					{dropdownOpen && (
-						<div className="absolute right-0 mt-2 w-40 border rounded shadow-lg z-10">
-							<Link href={"/profile"}>
-								<button className="block w-full text-left px-4 py-2 hover:bg-gray-100">
-									Profile
+				{isSignedIn ? (
+					<div className="relative">
+						<button
+							onClick={toggleDropdown}
+							className="w-10 h-10 rounded-full bg-blue-600 text-white flex items-center justify-center font-bold"
+						>
+							{userInitials}
+						</button>
+
+						{dropdownOpen && (
+							<div className="absolute right-0 mt-2 w-40 border rounded shadow-lg z-10">
+								<Link href={"/profile"}>
+									<button className="block w-full text-left px-4 py-2 hover:bg-gray-100">
+										Profile
+									</button>
+								</Link>
+								<button
+									onClick={onSignOut}
+									className="block w-full text-left px-4 py-2 hover:bg-gray-100 text-red-600"
+								>
+									Sign Out
 								</button>
-							</Link>
-							<button
-								onClick={onSignOut}
-								className="block w-full text-left px-4 py-2 hover:bg-gray-100 text-red-600"
-							>
-								Sign Out
-							</button>
-						</div>
-					)}
-				</div>
-			) : (
-				// Not yet signed in; display 'Sign In' button
-				<Link href={"/signin"}>
-					<button className="rounded bg-blue-600 px-3 py-1 text-white hover:bg-blue-700 cursor-pointer">
-						Sign In
-					</button>
-				</Link>
-			)}
+							</div>
+						)}
+					</div>
+				) : (
+					// Not yet signed in; display 'Sign In' button
+					<Link href={"/signin"}>
+						<button className="rounded bg-blue-600 px-3 py-1 text-white hover:bg-blue-700 cursor-pointer">
+							Sign In
+						</button>
+					</Link>
+				)}
+			</div>
 		</header>
 	);
 }
