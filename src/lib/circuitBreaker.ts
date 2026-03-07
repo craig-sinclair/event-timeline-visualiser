fc8ed9533aa26500c35d936f0b3d0c3779f51bb2
craig-sinclair <craigsincy152@gmail.com>
2026-03-07 17:37:30 +0000

    Define call (with timeout) in circuit breaker, and handle success/failures

--- src/lib/circuitBreaker.ts
+++ src/lib/circuitBreaker.ts
@@ -10,7 +10,7 @@ class CircuitBreaker {
 	private successCount = 0;
 	private lastFailureTime?: number;
 
-	constructor(private readonly _params: CircuitBreakerParams = {}) {}
+	constructor(private readonly params: CircuitBreakerParams = {}) {}
 
 	getState(): CircuitBreakerInternalState {
 		return {
@@ -26,6 +26,54 @@ class CircuitBreaker {
 		this.failureCount = 0;
 		this.successCount = 0;
 		this.lastFailureTime = undefined;
+	}
+
+	async call<T>(fn: () => Promise<T>): Promise<T> {
+		if (this.status == "OPEN") {
+			const elapsedTimeSinceFailure = Date.now() - (this.lastFailureTime ?? 0);
+			if (elapsedTimeSinceFailure < (this.params.retryTimeoutMs ?? 30_000)) {
+				throw new Error("Request rejected, circuit breaker is OPEN.");
+			}
+			this.status = "HALF_OPEN";
+		}
+
+		try {
+			const result = await this.withTimeout(fn);
+			this.onSuccess();
+			return result;
+		} catch (err) {
+			this.onFailure();
+			throw err;
+		}
+	}
+
+	private withTimeout<T>(fn: () => Promise<T>): Promise<T> {
+		const msTimeout = this.params.callTimeoutMs ?? 8_000;
+		return Promise.race([
+			fn(),
+			new Promise<never>((_, reject) =>
+				setTimeout(() => reject(new Error(`Timeout after ${msTimeout}ms`)), msTimeout)
+			),
+		]);
+	}
+
+	private onSuccess() {
+		this.failureCount = 0;
+		if (this.status == "HALF_OPEN") {
+			this.successCount++;
+			if (this.successCount >= (this.params.successThreshold ?? 3)) {
+				this.status = "CLOSED";
+				this.successCount = 0;
+			}
+		}
+	}
+
+	private onFailure() {
+		this.failureCount++;
+		this.lastFailureTime = Date.now();
+		if (this.failureCount >= (this.params.failureThreshold ?? 5)) {
+			this.status = "OPEN";
+		}
 	}
 }
 
